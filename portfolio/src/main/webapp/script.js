04a5cd153d405bcced960ba8c4cc3717f7994747
KatherineKee <theleafgecko@gmail.com>
2020-06-09 00:42:45 +0800

    moved js to script.js

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -37,3 +37,18 @@ function navFunction() {
     x.className = "topnav";
   }
 }
+
+// Automatic Slideshow - change image every 3 seconds
+var myIndex = 0;
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("my-slides");
+    for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}
+    x[myIndex-1].style.display = "block";
+    setTimeout(carousel, 3000);
+}
